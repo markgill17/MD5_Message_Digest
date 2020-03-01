a13afd9e53cb366da1463409fddd827dcfdb4a94
markgill <G00348787@gmit.ie>
2020-03-01 00:54:10 +0000

    padding.c reads files

--- tutorials/sha256/padding.c
+++ tutorials/sha256/padding.c
@@ -4,5 +4,28 @@
 
 int main(int argc, char* argv){
 
+    if (argc != 2){
+        printf("Error: Expecting single filename as argument\n");
+        return 1;
+    }
+
+    FILE *infile = fopen(argv[1], "rb");
+
+    if(!infile){
+        printf("Error: Could not open file %s\n", argv[1]);
+        return 1;
+    }
+
+    uint8_t b;
+
+    //  '&' is the address of b not the value of b
+    for(;fread(&b, 1, 1, infile) == 1;){
+        printf("%02x", b);
+    }
+
+    printf("\n");
+
+    fclose(infile);
+
     return 0;
 }
