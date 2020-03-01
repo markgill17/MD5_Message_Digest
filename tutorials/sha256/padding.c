1c3669622d11019e98861067da3b3a9c5756c229
markgill <G00348787@gmit.ie>
2020-03-01 01:00:04 +0000

    adds zero bytes for messaging block

--- tutorials/sha256/padding.c
+++ tutorials/sha256/padding.c
@@ -2,6 +2,19 @@
 // includes formatters for printing 64 bit integers
 #include<inttypes.h>
 
+uint64_t NoZerosBytes(uint64_t nobits){
+    // the number of bits from the file
+    uint64_t result = 512ULL - (nobits % 512ULL);
+
+    // find out if there is enough room in the last block to do the padding or will a new block be needed
+    if (result < 65)// 65 64(length integer and the 1)
+        result += 512;
+
+    // get the number of 0 bytes needed to pad between the 1 and the 64 bit integer
+    result -=72;
+    return (result/ 8ULL);
+}
+
 int main(int argc, char* argv){
 
     if (argc != 2){
@@ -19,11 +32,18 @@ int main(int argc, char* argv){
     uint8_t b;
 
     //  '&' is the address of b not the value of b
-    for(;fread(&b, 1, 1, infile) == 1;){
+    // nobits += 8 gets the number of bits read
+    for(;fread(&b, 1, 1, infile) == 1; nobits += 8){
         printf("%02x", b);
     }
 
-    printf("\n");
+    // this is appended to the message block
+    printf("%02", PRIx8, 0x80);
+
+    for(uint64_t i = NoZerosBytes(nobits); i > 0; i--)
+        printf("%02" PRIx8, 0x00);
+    // prints the number of bits to the screen
+    printf("%016" PRIx64 "\n", nobits);
 
     fclose(infile);
 
