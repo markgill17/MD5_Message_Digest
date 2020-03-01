aa880e2469a1b59933bd86c4ce8357a817aa0ead
markgill <G00348787@gmit.ie>
2020-03-01 01:35:11 +0000

    separated contents of main and added uint32_t K[]

--- tutorials/sha256/sha256.c
+++ tutorials/sha256/sha256.c
@@ -34,13 +34,16 @@ uint32_t Sig3(uint32_t x){
     return ROTR(x, 17) ^ ROTR(x, 19) ^ SHR(x, 10);
 }
 
-int main(int argc, char *argv[]){
-    return 0;
-}
-
-#include<stdio.h>
-// includes formatters for printing 64 bit integers
-#include<inttypes.h>
+const uint32_t K[] = {
+        0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+        0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+        0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+        0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+        0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+        0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+        0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+        0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f
+};
 
 union block{
     uint64_t sixFour[8];
@@ -61,13 +64,14 @@ uint64_t NoZerosBytes(uint64_t nobits){
     return (result/ 8ULL);
 }
 
-int main(int argc, char* argv){
-
+int main(int argc, char* argv[]){
     if (argc != 2){
         printf("Error: Expecting single filename as argument\n");
+        // if main returns anything other then 0, error thrown
         return 1;
     }
 
+    // declare for reading file
     FILE *infile = fopen(argv[1], "rb");
 
     if(!infile){
@@ -75,28 +79,36 @@ int main(int argc, char* argv){
         return 1;
     }
 
-    uint8_t b;
-
-    uint64_t nobits;
-
     union block M;
-    uint8_t i;
 
-    //  '&' is the address of b not the value of b
-    // nobits += 8 gets the number of bits read
-    for(;fread(&b, 1, 1, infile) == 1; nobits += 8){
-        printf("%02" PRIx8, M.eight[i]);
+
+    const uint32_t H[] = {
+            0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
+            0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
+    };
+
+    while (M = nextblock(&M, infile)){
+        H = nexthash(M, H);
     }
-
-    // this is appended to the message block
-    printf("%02", PRIx8, 0x80);
-
-    for(uint64_t i = NoZerosBytes(nobits); i > 0; i--)
-        printf("%02" PRIx8, 0x00);
-    // prints the number of bits to the screen
-    printf("%016" PRIx64 "\n", nobits);
 
     fclose(infile);
 
     return 0;
 }
+
+int nextblock(union block *M, FILE *infile){
+
+    uint64_t nobits;
+    uint8_t i;
+
+    for(nobits = 0, i = 0; fread(&M.eight[i], 1, 1, infile) == 1; nobits += 8){
+
+        // Appending the bits
+        printf("%02", PRIx8, 0x80);	// bits : 1000 0000
+
+        for(uint64_t i = NoZerosBytes(nobits); i > 0; i--)
+            printf("%02" PRIx8, 0x00);
+
+        // prints the number of bits to the screen
+        printf("%016" PRIx64 "\n", nobits);
+    }
